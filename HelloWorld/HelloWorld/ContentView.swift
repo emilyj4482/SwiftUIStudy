d642480b6f9aa57cab0b7ebdf8282aec974dc64a
emilyj4482 <emilyj4482@gmail.com>
2025-01-09 20:47:23 +0900

    feat: TextView 추가

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -19,15 +19,27 @@ struct ContentView: View {
 
 struct YellowView: View {
     var body: some View {
-        HStack {
+        HStack(spacing: 20) {
             Image(systemName: "message.circle.fill")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 80, height: 80)
+            
+            TextView()
         }
         .padding(.leading, 16)
         .frame(maxWidth: .infinity, alignment: .leading)
         .background(Color.yellow)
+    }
+}
+
+struct TextView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Hello")
+            
+            Text("World")
+        }
     }
 }
 
