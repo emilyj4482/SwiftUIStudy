731ac9f1cb2d04ce32c9c8fa59109865c3711035
emilyj4482 <emilyj4482@gmail.com>
2025-01-09 21:34:38 +0900

    feat: TextView 높이에 따라 padding과 alignment가 적용되도록 구현

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -20,18 +20,52 @@ struct ContentView: View {
 struct YellowView: View {
     let textCounts: (Int, Int)
     
+    @State var imageHeight: Double = 0.0
+    @State var textHeight: Double = 0.0
+    
+    @State var isTextHigher: Bool = false
+    
     var body: some View {
-        HStack(spacing: 20) {
-            Image(systemName: "message.circle.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80, height: 80)
+        HStack(alignment: isTextHigher ? .center : .top, spacing: 20) {
+            
+            ImageView()
+                .overlay {
+                    GeometryReader { geometry in
+                        Color.clear.onAppear {
+                            print(geometry.size.height)
+                            imageHeight = geometry.size.height
+                            isTextHigher = textHeight > imageHeight
+                            print(isTextHigher)
+                        }
+                    }
+                }
             
             TextView(helloCount: textCounts.0, worldCount: textCounts.1)
+                .overlay {
+                    GeometryReader { geometry in
+                        Color.clear.onAppear {
+                            print(geometry.size.height)
+                            textHeight = geometry.size.height
+                            isTextHigher = textHeight > imageHeight
+                            print(isTextHigher)
+                        }
+                    }
+                }
         }
         .padding(.leading, 16)
+        .padding([.top, .bottom], isTextHigher ? 10 : 20)
         .frame(maxWidth: .infinity, alignment: .leading)
         .background(Color.yellow)
+    }
+    
+}
+
+struct ImageView: View {
+    var body: some View {
+        Image(systemName: "message.circle.fill")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 80, height: 80)
     }
 }
 
