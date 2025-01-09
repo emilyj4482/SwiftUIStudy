132902ece483191b62250e8ef3ad9667ccf6fff6
emilyj4482 <emilyj4482@gmail.com>
2025-01-09 20:28:14 +0900

    chore: 프로젝트 생성

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by EMILY on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
